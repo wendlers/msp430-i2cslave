eb1765f1a57809ecd4ee82441b2fbac1cdca1388
Stefan Wendler <sw@kaltpost.de>
2013-05-28 21:08:49 +0200

    does not work as expected

--- sigrok.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-##
-# Probe with OLS (pin 0->SDA, 1->SCL)
-##
-
-DRIVER=ols:conn=/dev/ttyACM1
-
-sigrok-cli --driver $DRIVER --config samplerate=1m --probes 0,1 --time 1s -P i2c:sda=0:scl=1
